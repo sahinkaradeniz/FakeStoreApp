9d665cb6af9b89cab40da17537069a470f6902af
Şahin Karadeniz <sahin.karadeniz@appcent.mobi>
2025-01-17 00:05:53 +0300

    cart fix

--- presentation/basket/src/main/java/com/skapps/basket/BasketScreen.kt
+++ presentation/basket/src/main/java/com/skapps/basket/BasketScreen.kt
@@ -2,30 +2,55 @@ package com.skapps.basket
 
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.unit.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.skapps.fakestoreapp.core.loading.LoadingType
 import com.skapps.fakestoreapp.coreui.R
 import com.skapps.fakestoreapp.coreui.components.AppTopBar
 import com.skapps.fakestoreapp.coreui.components.LoadImageFromUrl
 import com.skapps.fakestoreapp.coreui.theme.CollectSideEffect
-import com.skapps.fakestoreapp.core.loading.LoadingType
+import com.skapps.fakestoreapp.coreui.theme.Purple80
 
 @Composable
 fun BasketScreen(viewModel: BasketViewModel = hiltViewModel()) {
@@ -212,7 +237,7 @@ fun BasketSummary(price: Double, discount: Double, total: Double, onCheckoutClic
                 .padding(16.dp)
                 .height(50.dp),
             shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+            colors = ButtonDefaults.buttonColors(containerColor = Purple80)
         ) {
             Text("CHECKOUT", fontSize = 16.sp, color = Color.Black)
         }
